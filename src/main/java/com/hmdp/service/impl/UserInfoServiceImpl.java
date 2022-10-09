11fb5bcca07167fd6a9fadf48f20cf085a4b3d57
rth <1920716851@qq.com>
2022-10-09 22:11:06 +0800

    发送短信功能修改，生成的验证码不再被保存到session中，而是存储到redis中

--- src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
@@ -1,17 +1,20 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
-import com.hmdp.dto.LoginFormDTO;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.mapper.UserInfoMapper;
 import com.hmdp.service.IUserInfoService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -25,6 +28,9 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements IUserInfoService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Result sendCode(String phone, HttpSession session) {
         // 1.验证手机号, 如果不符合、返回错误信息, 符合，生成验证码
@@ -32,9 +38,10 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         if (phoneInvalid) {
             return Result.fail("手机号码格式错误！");
         }
-        // 2. 生成验证码，保存验证码到session
+        // 2. 生成验证码，并将验证码保存到redis中
         String code = RandomUtil.randomNumbers(6);
-        session.setAttribute("code", code);
+        stringRedisTemplate.opsForValue().set(RedisConstants.LOGIN_CODE_KEY + phone, code, RedisConstants.LOGIN_CODE_TTL, TimeUnit.SECONDS);
+
         // 3. 发送验证码
         log.debug("发送短信验证码成功，验证码为：{}", code);
         return Result.ok();
